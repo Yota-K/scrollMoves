b58c982881c86c551e7b17d0dba9f809d8d78729
紅梅陽太 <powdersugar828828@gmail.com>
2020-12-09 22:55:13 +0900

    webpackが自分のローカルだと動かなかったので修正。

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+const path = require('path');
+
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "development",
+  // package.jsonに移動しました
+  // mode: "development",
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: "./src/index.ts",
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
-    path: `${__dirname}/dist`,
+    path: path.resolve(__dirname, 'dist'),
     // 出力ファイル名
     filename: "bundle.js",
   },
